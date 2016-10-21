8ab3b608e3d4e9b53b2624f15cc634a9e25469ae
Kyle Miller <miller.kwill@gmail.com>
2016-10-20 22:55:09 -0500

    initial commit of happy project

---
+++ public/app.js
@@ -0,0 +1,19 @@
+(function() {
+angular.module('happyApp', [])
+  .controller('projectHappyCtl', projectHappyController)
+
+  function projectHappyController() {
+    var this = 
+    var fromAddress = {
+      name: "Satcom Direct",
+      street1:"1050 Satcom Lane",
+      street2:"",
+      city:"Melbourne",
+      state:"FL",
+      zip:"32940",
+      country:"US",
+      phone:"613-862-4225"
+    }
+
+
+}());
