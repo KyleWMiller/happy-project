d47a2865eb4b9958e2ebaaea25ba384f83dbd1fc
Lanelle Miller <miller.kwill@gmail.com>
2017-03-22 13:21:57 -0600

    add heroku mlab

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ apiRoutes = require('./apiRoutes.js'),
 
     mongoose = require('mongoose'),
     // Mongoose is used for backend validation of information going to MongoDB
+    // Local DB link
     // databaseURL = "mongodb://localhost:27017/hsdb,"
-    // Mlab Db link
-    databaseURL = "mongodb://Jerry:comedianporche@ds135790.mlab.com:35790/hsdb"
+    // Mlab DB link
+    databaseURL = "mongodb://Jerry:comedianporche@ds139470.mlab.com:39470/heroku_fqsdtdjx"
     mongoose.Promise = global.Promise
     mongoose.connect(databaseURL, function(err) {
       if (err) {
